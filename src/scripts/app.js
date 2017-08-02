e5b206d0e10fbb351c5df9a88c3604633f12f226
Kathryn Buchanan <kathrynb1213@gmail.com>
2017-08-02 11:33:20 -0500

    Add slant

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-    var mySwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
-      direction: 'vertical',
-      loop: true
-    }); 
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+
+    function changeStyle() {
+    var x = document.getElementById("selectStyle").value;
+    document.getElementById("container").className = "" + x;
+}
+
+
 });
